576ea4a87595de41debbd35db15161b481e2c3ac
mitaksim1 <miriamt.simonnet@gmail.com>
2021-09-07 15:43:35 +0200

    Création de la première route /teste

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,13 @@ import express from "express";
 // 2. Appel à Express
 const app = express();
 
+// 4. Création de la première route
+// Paramètres attendus : ressource/route
+app.get("/teste", (request, response) => {
+    // Request = requêtes client
+    // Response = réponse serveur
+    return response.send("Hello World");
+});
+
 // 3. Installer le typage d'Express pour avoir l'auto-complétion de toutes les fonctonnalités : yarn add @types/express -D
 app.listen(3000, () => console.log("Server is running"));
