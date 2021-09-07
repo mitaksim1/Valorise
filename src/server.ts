5cdb5c0c87396c4f12ab7689238d140f1aa794f1
mitaksim1 <miriamt.simonnet@gmail.com>
2021-09-07 16:04:04 +0200

    Création d'une route avec la méthode POST

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,15 @@ const app = express();
 
 // 4. Création de la première route
 // Paramètres attendus : ressource/route
-app.get("/teste", (request, response) => {
+app.get("/test", (request, response) => {
     // Request = requêtes client
     // Response = réponse serveur
     return response.send("Hello World");
+});
+
+// 5. Création de la première route en POST
+app.post("/test-post", (request, response) => {
+    return response.send("Teste en POST");
 });
 
 // 3. Installer le typage d'Express pour avoir l'auto-complétion de toutes les fonctonnalités : yarn add @types/express -D
