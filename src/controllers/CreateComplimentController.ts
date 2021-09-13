4645dfe376bc81875f98be9d817152a548f9204d
mitaksim1 <miriamt.simonnet@gmail.com>
2021-09-13 16:23:36 +0200

    Récupération de l'id de l'utilisateur pour valider la création d'un compliment

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -4,13 +4,15 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentController {
     async handle(request: Request, response: Response) {
-        const { tag_id, user_sender, user_receiver, message } = request.body;
+        const { tag_id, user_receiver, message } = request.body;
+        // En récupérant le user_id, l'utilisateur qui voudra créer un compliment sera obligé d'être connecté et authentifié
+        const { user_id } = request;
 
         const createComplimentService = new CreateComplimentService();
 
         const compliment = await createComplimentService.execute({
             tag_id, 
-            user_sender, 
+            user_sender: user_id, 
             user_receiver, 
             message
         });
