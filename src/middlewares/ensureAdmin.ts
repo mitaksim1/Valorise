787ca76d775eafbc7791b9ffe0e2ea3ee420df36
mitaksim1 <miriamt.simonnet@gmail.com>
2021-09-13 16:11:16 +0200

    Récupération de l'id de l'utilisateur pour vérifier si il est admin ou pas + teste Insomnia (autentification + création tag)

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,14 +1,19 @@
 // Middleware à être utilisé dans une route
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-    // Vérifie si l'utilisateurs est admin
-    const admin = true;
-
-    // Récupérer l'id de l'utilisateur
+export async  function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    // Récupérer l'id de l'utilisateur pour savoir s'il est admin ou pas
     const { user_id } = request;
     console.log(user_id);
 
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    // Destructurer la variable 'user' pour récupérer directement l'information que l'on veut
+    const { admin } = await usersRepositories.findOne(user_id);
+
+    // Vérifie si l'utilisateur est admin
     if (admin) {
         return next();
     }
